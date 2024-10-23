3e3b0813bc71c6f79045f726654223850e321db0
Aleksandr Pankov <nayteruz@gmail.com>
2024-10-24 01:34:13 +0700

    fix store decorator

--- src/app/providers/StoreProvider/ui/StoreProvider.tsx
+++ src/app/providers/StoreProvider/ui/StoreProvider.tsx
@@ -1,5 +1,6 @@
 import { FC, ReactNode } from 'react';
 import { Provider } from 'react-redux';
+import { ReducersMapObject } from '@reduxjs/toolkit';
 import { StateSchema } from '../config/StateSchema';
 import { DeepPartial } from '@/shared/lib';
 import { createReduxStore } from '../config/store';
@@ -7,13 +8,21 @@ import { createReduxStore } from '../config/store';
 interface IStoreProviderProps {
   children: ReactNode;
   initialState?: DeepPartial<StateSchema>;
+  asyncReducers?: DeepPartial<ReducersMapObject<StateSchema>>
 }
 
 export type RootState = ReturnType<ReturnType<typeof createReduxStore>['getState']>;
 export type AppDispatch = ReturnType<typeof createReduxStore>['dispatch'];
 
-export const StoreProvider: FC<IStoreProviderProps> = ({ children, initialState }) => {
-  const store = createReduxStore(initialState as StateSchema);
+export const StoreProvider: FC<IStoreProviderProps> = ({
+  children,
+  initialState,
+  asyncReducers,
+}) => {
+  const store = createReduxStore(
+    initialState as StateSchema,
+    asyncReducers as ReducersMapObject<StateSchema>,
+  );
 
   return (
     <Provider store={store}>
--- src/shared/config/storybook/StoryDecorator/index.tsx
+++ src/shared/config/storybook/StoryDecorator/index.tsx
@@ -1,9 +1,21 @@
 import { StoryFn } from '@storybook/react';
+import { ReducersMapObject } from '@reduxjs/toolkit';
 import { StateSchema, StoreProvider } from '@/app/providers/StoreProvider';
 import { DeepPartial } from '@/shared/lib';
+import { loginReducer } from '@/features/AuthByUserName/model/slice/loginSlice';
 
-export const StoryDecorator = (initialState: DeepPartial<StateSchema>) => (Story: StoryFn) => (
-  <StoreProvider initialState={initialState}>
+const defaultAsyncReducers: DeepPartial<ReducersMapObject<StateSchema>> = {
+  loginForm: loginReducer,
+};
+
+export const StoryDecorator = (
+  initialState: DeepPartial<StateSchema>,
+  asyncReducers?: ReducersMapObject<StateSchema>,
+) => (Story: StoryFn) => (
+  <StoreProvider
+    initialState={initialState}
+    asyncReducers={{ ...defaultAsyncReducers, ...asyncReducers }}
+  >
     <Story />
   </StoreProvider>
 );
