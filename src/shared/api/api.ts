ba466f1dc3ec9b2584e72457868e7ffd3ccd5f74
Aleksandr Pankov <nayteruz@gmail.com>
2024-12-01 15:01:13 +0700

    fix auth

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -4,9 +4,7 @@ import { STORAGE_KEYS } from '../const/storageKeys';
 export const $api = axios.create({ baseURL: __API__ });
 
 $api.interceptors.request.use((config) => {
-  if (config.headers.Authorization) {
-    config.headers.Authorization = localStorage.getItem(STORAGE_KEYS.AUTH) || '';
-  }
+  config.headers.Authorization = localStorage.getItem(STORAGE_KEYS.AUTH) || '';
 
   return config;
 });
