386062f94b0c62fa7fd8036678d5e558040ae240
Aleksandr Pankov <nayteruz@gmail.com>
2024-12-01 14:48:07 +0700

    fix api config

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -1,7 +1,12 @@
 import axios from 'axios';
 import { STORAGE_KEYS } from '../const/storageKeys';
 
-export const $api = axios.create({
-  baseURL: __API__,
-  headers: {Authorization: localStorage.getItem(STORAGE_KEYS.AUTH) || '',},
+export const $api = axios.create({ baseURL: __API__ });
+
+$api.interceptors.request.use((config) => {
+  if (config.headers) {
+    config.headers.Authorization = localStorage.getItem(STORAGE_KEYS.AUTH) || '';
+  }
+
+  return config;
 });
